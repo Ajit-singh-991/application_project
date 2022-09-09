1143141437ec1f9693460e164f3d2edb0a992746
chand <rathoreajit991@gmail.com>
2022-09-09 20:11:11 +0530

    day3 completed

---
+++ lib/pages/bottomNavigation/profilenav.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const <Widget>[
+        Image(
+          image: AssetImage('assets/images/login_image.png'),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Text(
+          'Welcome',
+          style: TextStyle(
+            color: Color.fromARGB(255, 161, 6, 204),
+            fontSize: 45.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          'To',
+          style: TextStyle(
+            color: Color.fromARGB(255, 198, 75, 232),
+            fontSize: 35.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          'Profile Page',
+          style: TextStyle(
+            color: Color.fromARGB(123, 151, 49, 179),
+            fontSize: 90.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
